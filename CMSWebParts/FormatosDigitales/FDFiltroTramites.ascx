c7580973919191db78a47da76a9ab9e8a6a0604e
GladysSanchez <gladys.sanchez@iica.int>
2016-05-26 14:02:44 -0500

    Commin FMOGOLLON 2:02

--- CMSWebParts/FormatosDigitales/FDFiltroTramites.ascx
+++ CMSWebParts/FormatosDigitales/FDFiltroTramites.ascx
@@ -1,23 +1,26 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="FDFiltroTramites.ascx.cs" Inherits="CMSWebParts_FormatosDigitales_FDFiltroTramites" %>
 <asp:Panel ID="PnlFiltro" runat="server">
 <div>
-    <asp:Label ID="Label1" runat="server" Text="lblProyectos"></asp:Label>
+    <asp:Label ID="Label1" runat="server" Text="Proyectos"></asp:Label>
     <asp:DropDownList ID="DdlProyectos" runat="server">
     </asp:DropDownList>
 </div>
 <div>
-    <asp:Label ID="Lbltramite" runat="server" Text="lblTramite"></asp:Label>
-    <asp:DropDownList ID="DdlTramites" runat="server" AutoPostBack="True">
+    <asp:Label ID="Lbltramite" runat="server" Text="Tramite"></asp:Label>
+    <asp:DropDownList ID="DdlTramites" runat="server" AutoPostBack="True" 
+        onselectedindexchanged="DdlTramites_SelectedIndexChanged" style="height: 22px">
     </asp:DropDownList>
-    <asp:Label ID="LblEstado" runat="server" Text="lblEstado"></asp:Label>
-    <asp:DropDownList ID="DdlEstados" runat="server">
+    <asp:Label ID="LblEstado" runat="server" Text="Estado"></asp:Label>
+    <asp:DropDownList ID="DdlEstados" runat="server" Enabled="False">
     </asp:DropDownList>
 </div>
 <div>
-    <asp:Label ID="LblFechaIni" runat="server" Text="LblFechaIni"></asp:Label>
+    <asp:Label ID="LblFechaIni" runat="server" Text="FechaIni"></asp:Label>
     <asp:TextBox ID="TxtFechaIni" runat="server"></asp:TextBox>
-    <asp:Label ID="LblFechaFin" runat="server" Text="Label"></asp:Label>
+    <asp:Label ID="LblFechaFin" runat="server" Text="FechaFIn"></asp:Label>
     <asp:TextBox ID="TxtFechaFin" runat="server"></asp:TextBox>
-    <asp:Button ID="BtnFiltrar" runat="server" Text="Button" />
+    <asp:Button ID="BtnFiltrar" runat="server" Text="Filtrar" 
+        onclick="BtnFiltrar_Click" />
+    <asp:Label ID="lblMensaje" runat="server" Text="Label"></asp:Label>
 </div>
 </asp:Panel>
--- CMSWebParts/FormatosDigitales/FDFiltroTramites.ascx.cs
+++ CMSWebParts/FormatosDigitales/FDFiltroTramites.ascx.cs
@@ -90,14 +90,12 @@ public partial class CMSWebParts_FormatosDigitales_FDFiltroTramites : CMSAbstrac
     protected void GetListadoTipoFormato()
     {
         DataSet Proyectos = new DataSet();
-        QueryDataParameters parameters1 = new QueryDataParameters();
-        parameters1.Add("@IdUsuario", this.UserIDSMI);
-        Proyectos = ConnectionHelper.ExecuteQuery("FD.FormatoDigital.ListadoTipoFormato", parameters1);
+        Proyectos = ConnectionHelper.ExecuteQuery("FD.FormatoDigital.ListadoTipoFormato", null);
 
         ListItem todos = new ListItem();
         todos.Value = "0";
         todos.Text = "Todos";
-        this.DdlProyectos.Items.Add(todos);
+        this.DdlTramites.Items.Add(todos);
 
         if (!DataHelper.DataSourceIsEmpty(Proyectos))
         {
@@ -108,16 +106,109 @@ public partial class CMSWebParts_FormatosDigitales_FDFiltroTramites : CMSAbstrac
                 ListItem proyecto = new ListItem();
                 proyecto.Value = ValidationHelper.GetString(p["ItemId"].ToString(), "");
                 proyecto.Text = ValidationHelper.GetString(p["Nombre"].ToString(), "");
-                this.DdlProyectos.Items.Add(proyecto);
+                this.DdlTramites.Items.Add(proyecto);
+            }
+        }
+    }
+
+
+    protected void GetListadoEstadoPorFormato(string idFormatoDigitalSeleccionado)
+    {
+        DataSet Proyectos = new DataSet();
+        QueryDataParameters parameters1 = new QueryDataParameters();
+        parameters1.Add("@IdFormatoDigitalSeleccionado", idFormatoDigitalSeleccionado);
+        Proyectos = ConnectionHelper.ExecuteQuery("FD.FD_EstadoFormatoDigital.GetListadoEstadoPorFormato", parameters1);
+
+        ListItem todos = new ListItem();
+        todos.Value = "0";
+        todos.Text = "Todos";
+        this.DdlEstados.Items.Add(todos);
+
+        if (!DataHelper.DataSourceIsEmpty(Proyectos))
+        {
+            foreach (DataRow p in Proyectos.Tables[0].Rows)
+            {
+                ListItem proyecto = new ListItem();
+                proyecto.Value = ValidationHelper.GetString(p["ItemId"].ToString(), "");
+                proyecto.Text = ValidationHelper.GetString(p["NombreEstado"].ToString(), "");
+                this.DdlEstados.Items.Add(proyecto);
             }
         }
     }
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        string idp = CMS.GlobalHelper.URLHelper.GetQueryValue(CMS.GlobalHelper.URLHelper.CurrentURL, "idp");
+        string idt = CMS.GlobalHelper.URLHelper.GetQueryValue(CMS.GlobalHelper.URLHelper.CurrentURL, "idt");
+        string  ide = CMS.GlobalHelper.URLHelper.GetQueryValue(CMS.GlobalHelper.URLHelper.CurrentURL, "ide");
+        string fi = CMS.GlobalHelper.URLHelper.GetQueryValue(CMS.GlobalHelper.URLHelper.CurrentURL, "fi");
+        string ff = CMS.GlobalHelper.URLHelper.GetQueryValue(CMS.GlobalHelper.URLHelper.CurrentURL, "ff");
+
+        if (!string.IsNullOrEmpty(idp))
+        {
+            this.lblMensaje.Text = "Mostrando resultados filtrados para [" + idp + "]";
+        }
+        else { this.lblMensaje.Text = ""; }
+
+        
+
         if (!this.Page.IsPostBack)
         {
             GetProyectoSMI();
+            GetListadoTipoFormato();
         } 
+    }
+
+
+    protected void DdlTramites_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        DdlEstados.Items.Clear();
+        if (DdlTramites.SelectedValue != "0")
+        {
+            DdlEstados.Enabled = true;
+            GetListadoEstadoPorFormato(DdlTramites.SelectedValue);
+        }
+        else
+        {
+            DdlEstados.Enabled = false;
+        }
+        
+    }
+
+    protected void BtnFiltrar_Click(object sender, EventArgs e)
+    {
+        string url = "";
+        string IDP = "";
+        string IDT = "";
+        string IDE = "";
+        string FI = "";
+        string FF = "";
+        url = CMS.GlobalHelper.URLHelper.CurrentURL;
+        url = CMS.GlobalHelper.URLHelper.RemoveQuery(url);
+            
+        IDP = DdlProyectos.SelectedValue;
+        IDT = DdlTramites.SelectedValue;
+        if (!string.IsNullOrEmpty(DdlEstados.SelectedValue))
+        {
+            IDE = DdlEstados.SelectedValue;
+        }
+        else {
+            IDE = "-1";  // cuando es negativo indica que no ha sido seleccionado el comboEstado
+        }
+        
+        FI  = TxtFechaIni.Text.Trim();        
+        FF  = TxtFechaFin.Text.Trim();
+
+        //if (IDP != "0" | IDT!= "0"| IDE != "0" | FI != "" | FF != "")               
+        //if (DdlProyectos.SelectedIndex != 0)
+
+        if (IDP != "0" | IDT != "0" | FI != "" | FF != "")  
+        {
+            url = url + "?idp=" + IDP + "&idt=" + IDT + "&ide=" + IDE + "&fi=" + FI + "&ff=" + FF;
+        }
+        this.Page.Response.Redirect(url);
+
+            
     }
 }
